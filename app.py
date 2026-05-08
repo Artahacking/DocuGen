5c86afa09ad545f9ed0ce1f827ceddfea76d0da1
Artahacking <128251302+Artahacking@users.noreply.github.com>
2026-05-08 22:21:24 +0700

    commit railway

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ app.secret_key = 'ganti_secret_key_ini'
 
 DB_CONFIG = {
     'host': os.getenv('MYSQLHOST'),
-    'port': int(os.getenv('MYSQLPORT')),
+    'port': int(os.getenv('MYSQLPORT', 3306)),
     'user': os.getenv('MYSQLUSER'),
     'password': os.getenv('MYSQLPASSWORD'),
     'database': os.getenv('MYSQLDATABASE')
